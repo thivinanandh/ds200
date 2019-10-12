e7b041f41ef538733c1cc3e887f4cbf33d3de1f1
thivinanandh <52538701+thivinanandh@users.noreply.github.com>
2019-10-12 16:52:21 +0530

    Update plot.py

--- plot.py
+++ plot.py
@@ -8,12 +8,16 @@ import matplotlib.pyplot as plt3
 plt.bar(data.iloc[-15:,0], data.iloc[-15:,5])
 plt.xlabel('Year')
 plt.title('Average RainFall in Southern Penninsula During Month of June-Sep' )
-plt.ylabel('RainFall in cm')
-plt.show()
+plt.ylabel('RainFall in mm')
+plt.grid('on', linestyle='--')
 
 
 
-plt2.scatter(data.iloc[-25:,0],data.iloc[-25:,10])
+plt2.scatter(data.iloc[-10:,0],data.iloc[-10:,4])
+plt.xlabel('Year')
+plt.ylabel('RainFall in mm')
+plt.title('Average RainFall in Southern Penninsula During Month of June-Sep' )
+plt.grid('on', linestyle='--')
+
 
 boxplot = data.boxplot(column = ['Actual Rainfall: JUN', 'Actual Rainfall: JUL','Actual Rainfall: AUG','Actual Rainfall: SEPT'])
-boxplot.title('Rainfall')
